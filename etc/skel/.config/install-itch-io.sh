e546298efd925cfb8a8a488c85d013d5866d96bd
UriHerrera <hello@uriherrera.com>
2022-04-01 02:46:30 -0600

    update pkg

--- etc/skel/.config/install-itch-io.sh
+++ etc/skel/.config/install-itch-io.sh
@@ -7,11 +7,16 @@ set -eu -o pipefail
 wget -q -O ~/.config/itch-setup https://raw.githubusercontent.com/Nitrux/storage/master/Other/itch-setup
 
 # -- Run setup.
+
+notify-send "Installing itch.io, please wait..." --icon=itch-io
+
 chmod +x ~/.config/itch-setup
 ~/.config/./itch-setup
 
 # -- Check if itch dir exists then delete setup file.
 
 [ -d ~/.itch ] && rm ~/.config/itch-setup ~/.config/install-itch-io.sh ~/.local/share/applications/install.itch.io.desktop
+
+notify-send "Installation complete." --icon=itch-io
 
 exit
--- etc/skel/.config/install-steam-flatpak.sh
+++ etc/skel/.config/install-steam-flatpak.sh
@@ -8,10 +8,14 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # -- Install Steam flatpak.
 
+notify-send "Installing Steam, please wait..." --icon=steam
+
 flatpak install -y flathub com.valvesoftware.Steam
 
 # -- Check if Steam flatpak dir exists then delete install launcher.
 
 [ -d ~/.var/app/com.valvesoftware.Steam ] && rm ~/.config/install-steam-flatpak.sh ~/.local/share/applications/install.steam-flatpak.desktop
+
+notify-send "Installation complete." --icon=steam
 
 exit
